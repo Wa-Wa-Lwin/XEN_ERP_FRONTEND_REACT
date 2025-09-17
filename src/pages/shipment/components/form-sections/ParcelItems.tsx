ecd01dbfdc2e48624c9c928564e8f8294c760a6a
ethan <ethan@xenoptics.com>
2025-09-17 09:48:45 +0700

    :art: remove rounded

--- src/pages/shipment/components/form-sections/ParcelItems.tsx
+++ src/pages/shipment/components/form-sections/ParcelItems.tsx
@@ -127,7 +127,7 @@ const ParcelItems = ({ parcelIndex, control, register, errors, setValue }: Parce
                 <Table
                     aria-label="Parcel items table"
                     classNames={{
-                        wrapper: "min-h-[200px] p-0",
+                        wrapper: "min-h-[200px] p-0 border-0 rounded-none",
                         table: "min-w-[1200px]",
                         td: "px-1 py-1",
                         th: "px-1 py-2",
