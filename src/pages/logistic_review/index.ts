154b0a16fd8f3c75a478baee0e36f563e53ba374
Wa Wa Lwin <wawa@xenoptics.com>
2025-09-19 16:51:22 +0700

    :fire: delete logistic review folder / will create a new one later

--- src/pages/logistic_review/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as LogisticReviewPage } from './LogisticReviewPage'
-export { default as LogisticReviewForm } from './components/LogisticReviewForm'
-export { default as LogisticReviewButton } from './components/LogisticReviewButton'
-export type * from './types/logistic-review.types'
