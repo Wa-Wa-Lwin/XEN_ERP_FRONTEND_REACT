0b8b5001cecce443e33adaee5f999422f46b7fee
Wa Wa Lwin <wawa@xenoptics.com>
2025-09-30 16:50:44 +0700

    :art: some wordings

--- src/pages/shipment/components/shipment-details/AddressInformation.tsx
+++ src/pages/shipment/components/shipment-details/AddressInformation.tsx
@@ -11,14 +11,24 @@ const AddressInformation = ({ shipment }: AddressInformationProps) => {
       <div className="space-y-0 border-r border-gray-200 pr-4">
         <h2 className="text-base font-semibold">Ship From</h2>
         <DetailRow label="Company" value={shipment.ship_from?.company_name} />
-        <DetailRow label="Address" value={`${shipment.ship_from?.street1}, ${shipment.ship_from?.city}, ${shipment.ship_from?.country}`} />
+        <DetailRow label="Street 1"  value={shipment.ship_from?.street1} />
+        <DetailRow label="Street 2"  value={shipment.ship_from?.street2} />
+        <DetailRow label="City"  value={shipment.ship_from?.city} />
+        <DetailRow label="Country"  value={shipment.ship_from?.country} />
+        <DetailRow label="State"  value={shipment.ship_from?.state} />
+        <DetailRow label="Postal Code"  value={shipment.ship_from?.postal_code} />
         <DetailRow label="Contact" value={`${shipment.ship_from?.contact_name} (${shipment.ship_from?.phone})`} />
         <DetailRow label="Email" value={shipment.ship_from?.email} />
       </div>
       <div className="space-y-0">
         <h2 className="text-base font-semibold">Ship To</h2>
         <DetailRow label="Company" value={shipment.ship_to?.company_name} />
-        <DetailRow label="Address" value={`${shipment.ship_to?.street1}, ${shipment.ship_to?.city}, ${shipment.ship_to?.country}`} />
+        <DetailRow label="Street 1"  value={shipment.ship_to?.street1} />
+        <DetailRow label="Street 2"  value={shipment.ship_to?.street2} />
+        <DetailRow label="City"  value={shipment.ship_to?.city} />
+        <DetailRow label="Country"  value={shipment.ship_to?.country} />
+        <DetailRow label="State"  value={shipment.ship_to?.state} />
+        <DetailRow label="Postal Code"  value={shipment.ship_to?.postal_code} />
         <DetailRow label="Contact" value={`${shipment.ship_to?.contact_name} (${shipment.ship_to?.phone})`} />
         <DetailRow label="Email" value={shipment.ship_to?.email} />
       </div>
