237f1b1c5089ff230996b91ff07641ff2d36486d
Wa Wa Lwin <wawa@xenoptics.com>
2025-10-27 17:16:03 +0700

    :construction:

--- src/pages/shipment/components/shipment-details/BasicInformation.tsx
+++ src/pages/shipment/components/shipment-details/BasicInformation.tsx
@@ -78,7 +78,7 @@ const BasicInformation = ({
   pickupCommonData = <>
     <h3
       className={`text-lg font-semibold mt-1 ${shipment.pick_up_created_status === "created_success"
-        ? "text-green-600"
+        ? "text-green-500"
         : shipment.pick_up_created_status === "created_success"
           ? "text-red-600"
           : "text-gray-700"
@@ -144,9 +144,6 @@ const BasicInformation = ({
   }
 
   let labelData = null;
-  // Extract Invoice link (just the URL string)
-  // const to_invoice = `${import.meta.env.VITE_APP_BACKEND_BASE_URL}${import.meta.env.VITE_APP_CUSTOMIZE_INVOICE_URL}${shipment.shipmentRequestID}`;
-  // const to_packing_slip = `${import.meta.env.VITE_APP_BACKEND_BASE_URL}${import.meta.env.VITE_APP_CUSTOMIZE_PACKING_SLIP_URL}${shipment.shipmentRequestID}`;
 
   if (shipment.approver_approved_date_time && (shipment.label_status === "created" || shipment.label_status === "cancelled")) {
     // Parse tracking numbers (comma-separated)
@@ -309,11 +306,11 @@ const BasicInformation = ({
 
   return (
     <>
-      <Card className="m-3 p-3">
+      <Card className="m-3 p-3  rounded-none">
         <div className="flex-1">
-          <div className="flex items-center gap-2 mb-2">
+          <div className="flex items-center gap-2">
             <Icon icon="solar:box-bold" width={20} className="text-blue-600" />
-            <h3 className="font-semibold text-blue-900">Basic Information</h3>
+            <h3 className="font-semibold text-blue-900">Action</h3>
             {canEdit && (
               <Button
                 color="primary"
@@ -339,8 +336,17 @@ const BasicInformation = ({
                 </Button>
               )}
           </div>
+        </div>
+      </Card>
+
+      <Card className="m-3 p-3 rounded-none">
+        <div className="flex-1">
+          <div className="flex items-center gap-2 mb-2">
+            <Icon icon="solar:box-bold" width={20} className="text-blue-600" />
+            <h3 className="font-semibold text-blue-900">Basic Information</h3>
+          </div>
           <div
-            className="grid gap-2 text-sm mb-3"
+            className="grid gap-2 text-sm"
             style={{
               gridTemplateColumns: 'repeat(8, max-content)',
               justifyContent: 'start',
@@ -420,59 +426,136 @@ const BasicInformation = ({
       </Card>
 
       {/* Pickup Information Card */}
-      <Card className="m-3 p-3">
-          <div className="flex items-center gap-2 mb-2">
-            <Icon icon="solar:calendar-bold" width={24} className={
-              shipment.pick_up_created_status === "created_success"
-                ? "text-green-600"
-                : shipment.pick_up_created_status === "created_failed"
-                  ? "text-red-600"
-                  : "text-blue-600"
-            } />
-            <h3 className={`font-semibold ${shipment.pick_up_created_status === "created_success"
-              ? "text-green-900"
+      <Card className="m-3 p-3 rounded-none">
+        <div className="flex items-center gap-2 mb-2">
+          <Icon icon="solar:calendar-bold" width={24} className={
+            shipment.pick_up_created_status === "created_success"
+              ? "text-green-600"
               : shipment.pick_up_created_status === "created_failed"
-                ? "text-red-900"
-                : "text-blue-900"
-              }`}>
-              Pickup Information ({shipment.pick_up_created_status === "created_success" ? "Success" : shipment.pick_up_created_status === "created_failed" ? "Failed" : "Pending"})
-            </h3>
+                ? "text-red-600"
+                : "text-blue-600"
+          } />
+          <h3 className={`font-semibold ${shipment.pick_up_created_status === "created_success"
+            ? "text-green-600"
+            : shipment.pick_up_created_status === "created_failed"
+              ? "text-red-600"
+              : "text-blue-600"
+            }`}>
+            Pickup Information ({shipment.pick_up_created_status === "created_success" ? "Success" : shipment.pick_up_created_status === "created_failed" ? "Failed" : "Pending"})
+          </h3>
+        </div>
+        <div
+          className="grid gap-2 text-sm mb-3"
+          style={{
+            gridTemplateColumns: 'repeat(8, fit-content(300px))',
+            justifyContent: 'start',
+            alignItems: 'start',
+            textAlign: 'left',
+          }}
+        >
+          <div>
+            <span className="text-gray-600">Date(Time): </span>
+            <span className="font-medium">
+              {pickupDateTime}
+            </span>
           </div>
-          <div
-            className="grid gap-2 text-sm mb-3"
-            style={{
-              gridTemplateColumns: 'repeat(8, max-content)',
-              justifyContent: 'start',
-              alignItems: 'start',
-              textAlign: 'left',
-            }}
-          >
-            <div>
-              <span className="text-gray-600">Date(Time): </span>
-              <span className="font-medium">
-                {pickupDateTime}
-              </span>
-            </div>
-            {shipment.pick_up_instructions !== "" &&
-              <>
-                <div>
-                  <span className="text-gray-600">| Instruction: </span>
-                  <span className="font-medium">
-                    {shipment.pick_up_instructions || '-'}
-                  </span>
-                </div>
-              </>
-            }
-            <div>
-              {isDHLeCommerceAsia && (
-              <div className="text-blue-600 font-semibold bg-blue-50 p-3 rounded border border-blue-200 mt-2">
-                <p>📞 Please call DHL eCommerce Asia customer service to arrange pickup for this package.</p>
-                <p>📇 Contact Logistic Team for further information.</p>
+          {shipment.pick_up_instructions !== "" &&
+            <>
+              <div>
+                <span className="text-gray-600">| Instruction: </span>
+                <span className="font-medium">
+                  {shipment.pick_up_instructions || '-'}
+                </span>
               </div>
-            )}
-            {pickupStatusData}
-            </div>
-          </div>     
+            </>
+          }
+          {
+            shipment.pick_up_created_status === "created_success" && shipment.pickup_confirmation_numbers !== "" &&
+            <>
+              <div>
+                <span className="text-gray-600"> | Confirmation No: </span>
+                <span className="font-medium">
+                  {shipment.pickup_confirmation_numbers}
+                </span>
+              </div>
+            </>
+          }
+          {
+            shipment.pick_up_created_status === "created_failed" &&
+            <>
+              <div>
+                <span className="text-gray-600"> | ⚠️ Error Detail: </span>
+                <span className="font-medium break-words max-w-full whitespace-normal">
+                  {formattedPickupError}
+                </span>
+              </div>
+
+              <Button
+                color="warning"
+                size="sm"
+                onPress={onChangePickupDateTime}
+              >
+                Change Pickup DateTime
+              </Button>
+              <Button
+                color="primary"
+                size="sm"
+                onPress={onCreatePickup}
+              // className="px-2 py-0 text-[11px] h-auto min-h-0"
+              >
+                Retry Create Pickup
+              </Button>
+
+            </>
+          }
+        </div>
+      </Card>
+
+      {/* Label Information Card */}
+      <Card className="m-3 p-3 rounded-none">
+        <div className="flex items-center gap-2 mb-2">
+          <Icon icon="solar:calendar-bold" width={24} className={
+            shipment.pick_up_created_status === "created_success"
+              ? "text-green-600"
+              : shipment.pick_up_created_status === "created_failed"
+                ? "text-red-600"
+                : "text-blue-600"
+          } />
+          <h3 className={`font-semibold ${shipment.label_status === "success"
+            ? "text-green-600"
+            : shipment.pick_up_created_status === "failed"
+              ? "text-red-600"
+              : "text-yellow-600"
+            }`}>
+            Label Information ({shipment.label_status === "success" ? "Success" : shipment.label_status === "failed" ? "Failed" : "Pending"})
+          </h3>
+        </div>
+        <div
+          className="grid gap-2 text-sm mb-3"
+          style={{
+            gridTemplateColumns: 'repeat(8, max-content)',
+            justifyContent: 'start',
+            alignItems: 'start',
+            textAlign: 'left',
+          }}
+        >
+          <div>
+            <span className="text-gray-600">Status(Time): </span>
+            <span className="font-medium">
+              {pickupDateTime}
+            </span>
+          </div>
+          {shipment.pick_up_instructions !== "" &&
+            <>
+              <div>
+                <span className="text-gray-600">| Instruction: </span>
+                <span className="font-medium">
+                  {shipment.pick_up_instructions || '-'}
+                </span>
+              </div>
+            </>
+          }
+        </div>
       </Card>
     </>
   );
